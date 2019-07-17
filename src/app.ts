bacda92cbdc446d123ee7d4bde8bd057e4c03c25
Jeff Gonzalez <jeff@Hypertheory>
2019-07-17 15:12:23 -0400

    done with game

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,43 @@
-import { getRandomInt } from "./utils";
+// import { getRandomInt } from "./utils";
 
 let squares: NodeList;
 let message: HTMLElement;
 export function runApp() {
-
-    squares = document.querySelectorAll('.square');
-    const secretSquareId = getRandomInt(1, 9); // TODO: actually make this random.
-    message = document.getElementById('message');
-    message.innerText = 'Play the Game!';
-
-    squares.forEach((square, index) => {
-        const that = square as HTMLElement;
-        that.classList.remove('winner', 'loser');
-        that.removeAttribute("data-secret");
-        if (index + 1 === secretSquareId) {
-            that.dataset.secret = "true";
-        } else {
-            // that.dataset.secret = "false";
-        }
-        square.addEventListener('click', handleClick)
+    let secretSquareId: number;
+    const promise = fetch('http://localhost:3000/game');
+    console.log('making your request');
+    promise.then((response) => {
+        response.json().then(x => {
+            secretSquareId = x.secret;
+            setUp();
+        })
     })
+
+    function setUp() {
+        squares = document.querySelectorAll('.square');
+        console.log(secretSquareId + 1);
+        message = document.getElementById('message');
+        message.innerText = 'Play the Game!';
+
+        squares.forEach((square, index) => {
+            const that = square as HTMLElement;
+            that.classList.remove('winner', 'loser');
+            that.removeAttribute("data-secret");
+            if (index + 1 === secretSquareId) {
+                that.dataset.secret = "true";
+            } else {
+                // that.dataset.secret = "false";
+            }
+            square.addEventListener('click', handleClick)
+        })
+    }
 }
 
 
 function handleClick(evt) {
     console.log({ evt, this: this });
     const that = this as HTMLElement;
-    if (that.dataset.secret) {
+    if (that.dataset.secret === "true") {
         that.classList.add('winner');
 
         message.innerText = 'You Win! (click here to play again!)';
